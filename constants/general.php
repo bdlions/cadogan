f61c132ac076594f2b417eb2f2249c3b8718ad9a
nazmulhasan <nazmul@apurbatech.com>
2015-11-21 11:26:18 +0600

    Image Upload Module is added

--- constants/general.php
+++ constants/general.php
@@ -4,3 +4,10 @@ define("SITE_TITLE",                                        "Cadogan");
 define("LOGO_TYPE_ID_HEADER",                               "1");
 define("LOGO_TYPE_ID_FOOTER",                               "2");
 
+define("IMAGE_UPLOAD_PATH",                                 "resources/images/");
+define("GALLERY_IMAGE_WIDTH",                               "100");
+define("GALLERY_IMAGE_HEIGHT",                              "100");
+define("LOGO_HEADER_WIDTH",                                 "100");
+define("LOGO_HEADER_HEIGHT",                                "100");
+define("LOGO_FOOTER_WIDTH",                                 "100");
+define("LOGO_FOOTER_HEIGHT",                                "100");
