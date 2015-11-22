b7f55abbf8a5a824244f22cb034e8de68325f731
nazmulhasan <nazmul@apurbatech.com>
2015-11-22 10:53:21 +0600

    home page info is updated from admin panel

--- application/models/org/admin/admin_home_model.php
+++ application/models/org/admin/admin_home_model.php
@@ -11,12 +11,20 @@ class Admin_home_model extends Ion_auth_model
     // -------------- Home page info module ------------------------//
     public function get_home_page_info()
     {
-        
+        return $this->db->select($this->tables['home_page_info'].'.id as home_page_info_id,'.$this->tables['home_page_info'].'.*')
+                ->from($this->tables['home_page_info'])
+                ->get();
     }
     
-    public function update_home_page_info()
+    public function update_home_page_info($home_page_info_id, $additional_data)
     {
-        
+        $data = $this->_filter_data($this->tables['home_page_info'], $additional_data);
+        $this->db->where('id', $home_page_info_id);
+        $this->db->update($this->tables['home_page_info'], $data);
+        if ($this->db->affected_rows() == 0) {
+            return FALSE;
+        }
+        return TRUE;
     }
     
     //--------------- Homa page links module ----------------//
