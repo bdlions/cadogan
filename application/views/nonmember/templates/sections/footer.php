d3efb9e2ab54d90f06839a8074222eb4171c7a30
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2015-12-02 15:12:55 +0600

    footer image bug fixing

--- application/views/nonmember/templates/sections/footer.php
+++ application/views/nonmember/templates/sections/footer.php
@@ -7,9 +7,9 @@
                         <?php foreach($logo_list as $logo_info){
                             if($logo_info['type_id'] == LOGO_TYPE_ID_FOOTER){
                         ?>
-                                <div class="col-md-3" style="padding-top:30px; padding-bottom: 30px;">
-                                    <img style="max-height:60px!important; max-width:250px!important;" class="img-responsive" src="<?php echo base_url()?>resources/images/<?php echo $logo_info['img']?>">
-                                </div>
+                        <ul class="center">
+                            <li><img style="max-height:60px!important; max-width:250px!important;" class="img-responsive" src="<?php echo base_url() ?>resources/images/<?php echo $logo_info['img'] ?>"></li>
+                         </ul>
                         <?php 
                            } 
                         } 
@@ -32,7 +32,8 @@
             <div class="row" >
                 <div class="col-md-offset-1 col-md-10">
                     <div class="slogan">
-                        <p><?php echo $home_page_info['footer_message']?></p>
+                        <p>Hello world</p>
+                        <p><?php // echo $home_page_info['footer_message']?></p>
                     </div>
                 </div>
             </div>
