82f356c5017af807721b440e15e34de30d7e0be9
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-04-11 22:47:12 +0600

    bug fixing

--- application/views/nonmember/page.php
+++ application/views/nonmember/page.php
@@ -1,19 +1,19 @@
 <!--commercial content-->
-                   
+
 <div class="row">
-    <?php $this->load->view('nonmember/templates/sections/left_panel');?>
-    <div class="col-md-9">
+    <?php $this->load->view('nonmember/templates/sections/left_panel'); ?>
+    <div class="col-xs-12 col-sm-12 col-md-9 text-justify">
         <div class="row">
             <div class="col-md-12">
-                <h4><?php echo $page_info['title']?></h4>
+                <h4><?php echo $page_info['title'] ?></h4>
             </div> 
         </div>
-        <?php if(!empty($page_info['img'])){ ?>
-        <div class="row form-group">
-            <div class="col-md-12">
-                <img class="size-full-img img-responsive" alt="commercial-img" src="<?php echo base_url().'resources/images/'.$page_info['img']?>"/>
-            </div> 
-        </div>
+        <?php if (!empty($page_info['img'])) { ?>
+            <div class="row form-group">
+                <div class="col-md-12">
+                    <img class="size-full-img img-responsive" alt="commercial-img" src="<?php echo base_url() . 'resources/images/' . $page_info['img'] ?>"/>
+                </div> 
+            </div>
         <?php } ?>
         <div class="row">
             <div class="col-md-12">
@@ -22,5 +22,24 @@
                 </div>
             </div> 
         </div>
+    </div>
+    <div class="col-xs-12 col-sm-12">
+        <aside class="contact-card-bg contact-card-visibility-hidden-mobile-device">
+            <div class="contact">
+                <h5>Contact Us</h5>
+                <?php
+                if (!empty($address_info['telephone1'])) {
+                    echo "<p>" . $address_info['location1'] . " - " . $address_info['telephone1'] . "</p>";
+                }
+                if (!empty($address_info['telephone2'])) {
+                    echo "<p>" . $address_info['location2'] . " - " . $address_info['telephone2'] . "</p>";
+                }
+                if (!empty($address_info['telephone3'])) {
+                    echo "<p>" . $address_info['location3'] . " - " . $address_info['telephone3'] . "</p>";
+                }
+                ?>
+                <p><a class="cursor_pointer"><?php echo $address_info['email'] ?></a></p>
+            </div>				
+        </aside>
     </div>
 </div>
